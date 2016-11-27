9116c6869c0d5497fafd52620ecbc08de9d34355
jm-factorin <jm.factorin@klab.com>
2016-11-27 13:04:02 +0800

    fix date tinme

--- app/models.py
+++ app/models.py
@@ -36,10 +36,11 @@ def create_user_stream(sender, instance, created, **kwargs):
 def publish_criminal_record_stream(sender, instance, created, **kwargs):
     if created:
         stream_id = instance.user.userstream.stream_id
+        committed_at = str('%Y-%m-d %H:%M').format(instance.committed_at)
         data = str('|').join([
             instance.offense,
             str(instance.case_number),
             instance.case_status,
-            instance.committed_at,
+            str(committed_at),
         ])
         api.publish(stream_id, 'records', data.encode('utf-8').hex())
